8ccdbe98a96716f60d16f714cf73886b5f2a71c0
nordknight@gmail.com <nordknight@gmail.com>
2016-11-17 00:54:27 +0300

    Adding flash message when signup success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to Project's Accounting!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
